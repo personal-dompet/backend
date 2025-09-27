228a3fa7d41269e40dc0232a2a29a381b5679976
Lutfi <lutfihakim288@gmail.com>
2025-09-27 09:18:00 +0700

    chore: update config files for network hosting and file paths

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase-admin/auth';
 
 
 if (!getApps().length) {
-  const serviceAccount = await Bun.file('../service-account.json').json();
+  const serviceAccount = await Bun.file('service-account.json').json();
   initializeApp({
     credential: cert(serviceAccount),
   });
